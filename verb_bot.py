233762c4c018a22299324d2e8ff73893b742fff4
zatomis <zatomis@list.ru>
2024-04-11 16:02:43 +0300

    setup DialogFlow

--- verb_bot.py
+++ verb_bot.py
@@ -10,9 +10,26 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+env: Env = Env()
+env.read_env()
+token_bot = env('VERBBOT_TOKEN')
+project_id = env('PROJECT_ID')
+session_id = env('SESSION_ID')
+language_code = env('LANGUAGE_CODE')
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    from google.cloud import dialogflow
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=texts, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return str(response.query_result.fulfillment_text)
+
+
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
@@ -28,33 +45,17 @@ def help_command(update: Update, context: CallbackContext) -> None:
 
 
 def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    update.message.reply_text(detect_intent_texts(project_id, session_id, update.message.text, language_code))
 
 
 def main() -> None:
 
-    env: Env = Env()
-    env.read_env()
-    token_bot = env('VERBBOT_TOKEN')
     updater = Updater(token_bot)
-
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
-
-    # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
